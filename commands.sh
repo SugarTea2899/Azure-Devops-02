ae174afe7281ce10c38bca318b5dd0cc625e27f7
SugarTea2899 <quangthien.tiit@gmail.com>
2024-06-29 00:01:02 +0700

    update pandas

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 az vm create --resource-group Azuredevops --name my-agent-1 --image Ubuntu2204 --size Standard_DS1_v2 --data-disk-sizes-gb 20 --admin-username devopsagent --admin-password DevOpsAgent@123 --nsg-rule SSH --public-ip-sku Standard
-az webapp up --name thienflaskapp --resource-group Azuredevops --runtime PYTHON:3.9 --sku B1
+az webapp up --name thienflaskapp --resource-group Azuredevops --sku B1 --logs --runtime "PYTHON:3.9"
