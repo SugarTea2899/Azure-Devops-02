1b4c649719238c289e7cb27727b1161f2505dad3
SugarTea2899 <quangthien.tiit@gmail.com>
2024-06-28 17:03:49 +0700

    Update lint

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-
-    LOG.info("Scaling Payload: %s payload")
+    LOG.info("Scaling Payload: %s", payload)
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
@@ -21,9 +20,8 @@ def scale(payload):
 @app.route("/")
 def home():
     html = "<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+    return html
 
-# TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
     # Performs an sklearn prediction
@@ -32,16 +30,20 @@ def predict():
         # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
         clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except:
-        LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
+    except FileNotFoundError:
+        LOG.error("Model file not found")
+        return "Model not loaded", 500
+    except Exception as e:
+        LOG.error("Error loading model: %s", e)
+        return "Model not loaded", 500
 
     json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
+    LOG.info("JSON payload: %s", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
+    LOG.info("Inference payload DataFrame: %s", inference_payload)
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
+    LOG.info("Prediction: %s", prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
